f2c965b143892dbec5434ced5f539c8c76bbfa2f
FarooqProProgrammer <farooqayub646@gmail.com>
2022-12-19 19:53:40 +0500

    upda

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Provider } from 'react-redux';
 import './App.css';
 import Routing from './routes';
-import store from './redux/store';
+import {store,persistor} from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
 function App() {
   return (
     <Provider  store={store}>
        <PersistGate loading={null} persistor={persistor}>
-       <Routing/>
+         <Routing/>
        </PersistGate>
     </Provider>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import { auth } from '../../config/db'
 import {FacebookAuthProvider ,signInWithPopup} from "firebase/auth"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from '../../redux/Action/setuser'
 
 const Header = () => {
+
+  const userInfoDetail = useSelector(state => state.setUser.User)
+
+  console.log(userInfoDetail.Name);
+  console.log(userInfoDetail.photo);
+  
 
   const provider = new FacebookAuthProvider();
   const dispatch = useDispatch();
@@ -16,7 +22,11 @@ const Header = () => {
       // The signed-in user info.
       const user = result.user;
       console.log(user);
-      dispatch(setUser(user))
+      const userInfo = {
+        Name : user.displayName ,
+        photo : user.photoURL
+      }
+      dispatch(setUser(userInfo))
       setProfile(true)
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       const credential = FacebookAuthProvider.credentialFromResult(result);
@@ -41,17 +51,17 @@ const Header = () => {
   return (
     <div>
         <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+  <div className="container mx-auto flex justify-around items-center flex-wrap p-5 flex-col md:flex-row items-center">
     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
      
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
-    <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+    {/* <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <a className="mr-5 hover:text-gray-900">First Link</a>
       <a className="mr-5 hover:text-gray-900">Second Link</a>
       <a className="mr-5 hover:text-gray-900">Third Link</a>
       <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-    </nav>
+    </nav> */}
     
     {/* <button onClick={facebookLogin} className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
@@ -59,9 +69,10 @@ const Header = () => {
       </svg>
     </button> */}
 
-    {profile === true ?  <><p className='text-2xl font-black'>Name:</p><div className="img w-[90px] h-[60px] border-2 border-black" style={{ borderRadius: "100%" }}>
-
-          </div></>
+    {profile === true ?  <div className='flex'><p className='text-2xl font-black'>{userInfoDetail.Name}</p>
+    <div className="img w-[90px] h-[60px] border-2 border-black" style={{ borderRadius: "100%" }}>
+        <img className='w-full h-full rounded-full' src={userInfoDetail.photo} alt="" />
+      </div></div>
     :
 <button onClick={facebookLogin} className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
